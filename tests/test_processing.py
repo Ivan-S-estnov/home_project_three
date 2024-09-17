a50f866263c8a2ea88edec108240fa36c3d5eb1d
Ivan <jaloylee@yandex.ru>
2024-09-17 18:57:26 +0400

    fixes

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -5,9 +5,34 @@ import pytest
 from src.processing import filter_by_state, sort_by_date
 
 
-@pytest.fixture
-def test_filter_by_state(processing_input: list[dict[str, Any]], processing_result_1: list[dict[str, Any]]) -> Any:
-    assert filter_by_state(processing_input) == processing_result_1
+@pytest.mark.parametrize(
+    "user_list, user_state, expected",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "",
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        ),
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "state='CANCELED'",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+    ],
+)
+def test_filter_by_state(user_list: Any, user_state: Any, expected: Any) -> Any:
+    assert filter_by_state(user_list, user_state) == expected
 
 
 @pytest.mark.parametrize(
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,9 +3,21 @@ import pytest
 from src.widget import get_date, mask_account_card
 
 
-@pytest.fixture
-def test_mask_account_card(widget_input: str, widget_result: str) -> str:
-    assert mask_account_card(widget_input) == widget_result
+@pytest.mark.parametrize(
+    "card_account_info, masks",
+    [
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Счет 35383033474447895560", "Счет **5560"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+    ],
+)
+def test_mask_account_card(card_account_info: str, masks: str) -> str:
+    return mask_account_card(card_account_info)
 
 
 @pytest.mark.parametrize(
@@ -18,4 +30,9 @@ def test_mask_account_card(widget_input: str, widget_result: str) -> str:
     ],
 )
 def test_get_date(user_date: str, repair_date: str) -> str:
-    assert get_date(user_date) == repair_date
+    return get_date(user_date)
+
+
+if __name__=="__main__":
+    assert mask_account_card("card_account_info") == "masks"
+    assert get_date("user_date") == "repair_date"
