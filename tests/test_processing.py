fb69f8f35ffbe2bc75abf4bba7081289a87c7bab
Ivan <jaloylee@yandex.ru>
2024-09-17 16:46:58 +0400

    tests/test_widget.py fixes

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -5,68 +5,31 @@ import pytest
 from src.processing import filter_by_state, sort_by_date
 
 
+@pytest.fixture
+def test_filter_by_state(processing_input: list[dict[str, Any]], processing_result_1: list[dict[str, Any]]) -> Any:
+    assert filter_by_state(processing_input) == processing_result_1
+
+
 @pytest.mark.parametrize(
-    "list_dictionaries, state, expected",
+    "user_input, reverse_date, result",
     [
         (
             [
                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            "",
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            ]
         ),
         (
             [
                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            "state='CANCELED'",
-            [
                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ]
         ),
     ],
 )
-def test_filter_by_state(list_dictionaries: list[dict[str, Any]], state: str, expected: list[dict[str, Any]]) -> Any:
-    return filter_by_state(list_dictionaries, state) == expected
-
-
-@pytest.mark.parametrize(
-    "date_dictionaries, sorted_date, expected",
-    [
-        (
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            "",
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        ),
-        (
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            "state='CANCELED'",
-            [
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-        ),
-    ],
-)
-def test_sort_by_date(
-    date_dictionaries: list[dict[str, Any]], sorted_date: bool, expected: list[dict[str, Any]]
-) -> Any:
-    return sort_by_date(date_dictionaries, sorted_date) == expected
-
-
-if __name__ == "__main__":
-    assert test_filter_by_state("list_dictionaries", "state", "expected")
-    assert test_sort_by_date("date_dictionaries", "sorted_date", "expected")
+def test_sort_by_date(user_input: Any, reverse_date: Any, result: Any) -> Any:
+    assert sort_by_date(user_input, reverse_date) == result
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,25 +3,13 @@ import pytest
 from src.widget import get_date, mask_account_card
 
 
-@pytest.mark.parametrize(
-    "card_account_info, masks",
-    [
-        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
-        ("Счет 64686473678894779589", "Счет **9589"),
-        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
-        ("Счет 35383033474447895560", "Счет **5560"),
-        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
-        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
-        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
-        ("Счет 73654108430135874305", "Счет **4305"),
-    ],
-)
-def test_mask_account_card(card_account_info: str, masks: str) -> str:
-    return mask_account_card(card_account_info) == masks
+@pytest.fixture
+def test_mask_account_card(widget_input: str, widget_result: str) -> str:
+    assert mask_account_card(widget_input) == widget_result
 
 
 @pytest.mark.parametrize(
-    "user_time, repair_time",
+    "user_date, repair_date",
     [
         ("2018-07-11T02:26:18.671407", "11.07.2018"),
         ("2019-07-03T18:35:29.512364", "03.07.2019"),
@@ -29,10 +17,5 @@ def test_mask_account_card(card_account_info: str, masks: str) -> str:
         ("2018-10-14T08:21:33.419441", "14.10.2018"),
     ],
 )
-def test_get_date(user_time: str, repair_time: str) -> str:
-    return get_date(user_time) == repair_time
-
-
-if __name__ == "__main__":
-    assert test_mask_account_card("card_account_info", "masks")
-    assert test_get_date("user_time", "repair_time")
+def test_get_date(user_date: str, repair_date: str) -> str:
+    assert get_date(user_date) == repair_date
