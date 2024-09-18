37bfedb77caf1834da46b4d10c58b2cd3b32f6ef
Ivan <jaloylee@yandex.ru>
2024-09-18 13:39:10 +0400

    fixes

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.fixture
 def widget_input():
     return """Maestro 1596837868705199
@@ -22,3 +23,31 @@ Visa Classic 6831 98** **** 7658
 Visa Platinum 8990 92** **** 5229
 Visa Gold 5999 41** **** 6353
 Счет **4305"""
+
+
+@pytest.fixture
+def processing_input():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def processing_result_one():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+@pytest.fixture
+def processing_result_two():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,60 +1,11 @@
-from typing import Any
-
 import pytest
 
 from src.processing import filter_by_state, sort_by_date
 
 
-@pytest.mark.parametrize(
-    "user_list, user_state, expected",
-    [
-        (
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            "",
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        ),
-        (
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            "state='CANCELED'",
-            [
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-        ),
-    ],
-)
-def test_filter_by_state(user_list: Any, user_state: Any, expected: Any) -> Any:
-    assert filter_by_state(user_list, user_state) == expected
+def test_filter_by_state(processing_input, processing_result_two):
+    assert filter_by_state(processing_input) == processing_result_two
 
 
-@pytest.mark.parametrize(
-    "user_input, reverse_date, result",
-    [
-        (
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ]
-        ),
-        (
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            ]
-        ),
-    ],
-)
-def test_sort_by_date(user_input: Any, reverse_date: Any, result: Any) -> Any:
-    assert sort_by_date(user_input, reverse_date) == result
+def test_sort_by_date(processing_input, processing_result_one):
+    assert sort_by_date(processing_input) == processing_result_one
