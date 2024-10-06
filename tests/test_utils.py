65e23a3eda9772e73eb6e44684bbe4385a9dabe0
Ivan <jaloylee@yandex.ru>
2024-10-06 22:17:04 +0400

    fixes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,17 @@
-from src.utils import get_json_file, json_way
+import json
+from unittest.mock import MagicMock, patch
+from pathlib import Path
+from src.utils import get_json_file
 
 
-def test_get_json_file(utils_input):
-    assert get_json_file(json_way) == utils_input
+@patch("builtins.open")
+def test_get_json_file(mock_open: MagicMock, path_name: Path) -> None:
+    mock_file = mock_open.return_value.__enter__.return_value
+    mock_file.read.return_value = json.dumps([{"test", "test"}])
+    assert get_json_file(path_name) == [{"test", "test"}]
+   # mock_file.read.return_value = json.dumps({})
+    #assert get_json_file(path_name) == []
+  #  mock_file.read.return_value = json.dumps("testtest")
+   # assert get_json_file(path_name) == []
+   # mock_file.read.return_value = json.dumps("")
+    #assert get_json_file(path_name) == []
