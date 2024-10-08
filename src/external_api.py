dc75e306c4d4c717df56241e638e9e6eba3f2c73
Ivan <jaloylee@yandex.ru>
2024-10-08 18:03:13 +0400

    fixes

--- src/external_api.py
+++ src/external_api.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import Any, Iterator, SupportsFloat
+from typing import Any
 
 import requests
 from dotenv import load_dotenv
@@ -16,17 +16,20 @@ API_KEY = os.getenv("API_KEY")
 payment = get_json_file(file_path=BASE_DIR / "data" / "operations.json")
 
 
-def valuta_conversion(payment: Any, code):
-    """Функция обращается к внешнему API для получения текущего
-    курса валют и конвертации суммы операции в рубли"""
+def valuta_conversion(payment: Any):
+    """
+    Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях
+    Если транзакция была в USD или EUR, происходит обращение к внешнему API для
+    получения текущего курса валют и конвертации суммы операции в рубли
+    """
     valuta_code = payment["operationAmount"]["currency"]["code"]
     amount_check = payment["operationAmount"]["amount"]
     if valuta_code == "RUB":
-        return amount_check
+        return round(float(amount_check),2)
     elif valuta_code != "RUB":
         response = requests.get(f"https://currate.ru/api/?get=rates&pairs={valuta_code}RUB&key={API_KEY}")
         result = response.json()
-        return result["data"]["USDRUB"]
+        return round(float(amount_check) * float(result["data"]["USDRUB"]),2)
 
 
 convert_result = valuta_conversion(
@@ -35,26 +38,8 @@ convert_result = valuta_conversion(
         "state": "EXECUTED",
         "date": "2019-07-03T18:35:29.512364",
         "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
-    },
-    "RUB",
+    }
 )
 
+
 print(convert_result)
-
-
-def get_sum_amount() -> Iterator[Any]:
-    """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
-    for i in payment:
-        if len(i) > 0:
-
-            if i["operationAmount"]["currency"]["code"] != "RUB":
-                amount_valuta = i["operationAmount"]["amount"] * convert_result
-                return amount_valuta
-            else:
-                amount_rub = i["operationAmount"]["amount"]
-                return amount_rub
-
-
-
-
-print(get_sum_amount())
