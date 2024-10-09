8548fcc11e20ef6d65de14a4d552be099cb6dc57
Ivan <jaloylee@yandex.ru>
2024-10-09 18:20:55 +0400

    all done

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,10 +1,13 @@
 from unittest.mock import Mock, patch
-
 from src.external_api import valuta_conversion
 
 
 @patch("requests.get")
-def test_valuta_conversion(mock_get: Mock, transaction):
+def test_one_valuta_conversion(mock_get: Mock, transaction_one):
     mock_get.return_value.json.return_value = {"status": 200, "message": "test", "data": {"USDRUB": "99.99"}}
-    assert valuta_conversion(transaction) == 9999
+    assert valuta_conversion(transaction_one) == 9999
     mock_get.assert_called_once()
+
+
+def test_two_valuta_conversion(transaction_two):
+    assert valuta_conversion(transaction_two) == 31957.58
