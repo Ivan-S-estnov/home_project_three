fe94693d8c594ea98b934ba6a5586a875c5e7c3e
Ivan <jaloylee@yandex.ru>
2024-10-11 14:55:21 +0400

    src/utils.py done

--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,12 @@ import logging
 from pathlib import Path
 from typing import Optional
 
-logger = logging.getLogger("utils")
-logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("logs/utils.log")
-file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+utils_logger = logging.getLogger("utils")
+utils_logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/utils.log", mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
-logger.addHandler(file_handler)
+utils_logger.addHandler(file_handler)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 join_path = BASE_DIR / "data" / "operations.json"
@@ -19,21 +19,21 @@ def get_json_file(file_path: Path) -> Optional[list]:
     список словарей с данными о финансовых транзакциях"""
 
     try:
-        logger.info("Принимаем путь до JSON-файла")
+        utils_logger.info("Принимаем путь до JSON-файла")
         with open(file_path, "r", encoding="utf-8") as json_file:
             try:
                 transaction_list = json.load(json_file)
                 if isinstance(transaction_list, list):
-                    logger.info("Функция возвращает список словарей с данными о финансовых транзакциях")
+                    utils_logger.info("Функция возвращает список словарей с данными о финансовых транзакциях")
                     return transaction_list
                 else:
                     return []
             except json.JSONDecodeError as ex:
-                logger.error(f"Произошла ошибка: {ex}")
+                utils_logger.error(f"Произошла ошибка: {ex}")
                 return []
 
     except FileNotFoundError as ex:
-        logger.error(f"Файл не найден: {ex}")
+        utils_logger.error(f"Файл не найден: {ex}")
         return []
 
 
