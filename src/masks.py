d226f0023e26537593dd32445e7eb9e7d52e1896
Ivan <jaloylee@yandex.ru>
2024-10-11 12:40:25 +0400

    fixes

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,21 @@
+import logging
+from venv import logger
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename='masks.log',
+                    filemode='w')
+masks_logger = logging.getLogger('app.masks')
+
+
+
 def get_mask_card_number(card_numb: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
-    return f"{card_numb[:4]} {card_numb[4:6]}** **** {card_numb[-4:]}"
+    try:
+        masks_logger.info("Ввод номена карты")
+        return f"{card_numb[:4]} {card_numb[4:6]}** **** {card_numb[-4:]}"
+    except Exception as e:
+        masks_logger.error(f"Произошла ошибка: {e}")
 
 
 print(get_mask_card_number("7000792289606361"))
@@ -8,7 +23,11 @@ print(get_mask_card_number("7000792289606361"))
 
 def get_mask_account(mask_account: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
-    return f"**{mask_account[-4:]}"
+    try:
+        masks_logger.info("Ввод номера счета")
+        return f"**{mask_account[-4:]}"
+    except Exception as e:
+        masks_logger.error(f"Произошла ошибка: {e}")
 
 
 print(get_mask_account("73654108430135874305"))
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,14 @@
 import json
+import logging
 from pathlib import Path
 from typing import Optional
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/utils.log")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 join_path = BASE_DIR / "data" / "operations.json"
@@ -11,17 +19,21 @@ def get_json_file(file_path: Path) -> Optional[list]:
     список словарей с данными о финансовых транзакциях"""
 
     try:
+        logger.info("Принимаем путь до JSON-файла")
         with open(file_path, "r", encoding="utf-8") as json_file:
             try:
                 transaction_list = json.load(json_file)
                 if isinstance(transaction_list, list):
+                    logger.info("Функция возвращает список словарей с данными о финансовых транзакциях")
                     return transaction_list
                 else:
                     return []
-            except json.JSONDecodeError:
+            except json.JSONDecodeError as ex:
+                logger.error(f"Произошла ошибка: {ex}")
                 return []
 
-    except FileNotFoundError:
+    except FileNotFoundError as ex:
+        logger.error(f"Файл не найден: {ex}")
         return []
 
 
